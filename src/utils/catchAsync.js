1d9f0dfde4913a41e56cfc2e0e3b892c3e111978
DrCodeMonster <tarikeser@gmail.com>
2023-11-01 08:01:22 +0300

    bug fix

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,5 +1,12 @@
 const catchAsync = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  Promise.resolve(fn(req, res, next)).catch((err) => {
+    if (typeof next === 'function') {
+      next(err);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('Error:', err);
+    }
+  });
 };
 
 module.exports = catchAsync;
