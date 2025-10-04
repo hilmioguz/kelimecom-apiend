f945a35f273e196a8c83373b4f5a1a0eeafa0db2
hilmi oguz <hilmioguz@gmail.com>
2025-10-05 02:32:05 +0300

    buffermaxentry düzeltildi

--- src/config/config.js
+++ src/config/config.js
@@ -46,8 +46,6 @@ module.exports = {
       maxPoolSize: 10,        // Maximum number of connections in the pool
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferMaxEntries: 0,    // Disable mongoose buffering
-      bufferCommands: false,  // Disable mongoose buffering
     },
   },
   jwt: {
